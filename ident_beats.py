b70e797361ba62d714201cf5492abb46e73f0c2b
Matthew Cover <41007986+matthewncover@users.noreply.github.com>
2022-09-11 19:12:56 -0700

    change beat calc using rolling mean of abs()

--- ident_beats.py
+++ ident_beats.py
@@ -3,17 +3,12 @@ from sklearn.cluster import k_means
 
 import datetime as dt, time
 
-def play_beats(beat_midpoints, i):
-
-
-
-    pass
-
-def find_beats(stream_data_arr):
+def find_beats(stream_data_arr, return_df = False):
     """current time master fn
     """
 
     start = time.time()
+    stream_data_arr = stream_data_arr[1:]
 
     df = (
         pd.Series(
@@ -28,23 +23,45 @@ def find_beats(stream_data_arr):
 
     df["abs_db"] = abs(df.db)
 
-    beat_midpoints = loc_beat_midpoints(df)
+    df_abs_rolled = (
+        df.abs_db
+        .rolling(20).mean()
+        .dropna()
+        .reset_index()
+        .rename(columns={"index": "timestep"})
+    )
+
+    # beat_midpoints = loc_beat_midpoints(df)
+    beat_midpoints = loc_beat_midpoints(
+        df_abs_rolled, 
+        percentile=.993, 
+        jump_size_cutoff=1000
+        )
 
     # runtime_delta = (dt.datetime.now() - start).seconds
     runtime_delta = time.time() - start
 
-    return beat_midpoints, runtime_delta
+    if return_df:
+        return beat_midpoints, df
+
+    else:
+        return beat_midpoints, runtime_delta
 
 
-def loc_beat_midpoints(df, jump_size_cutoff=1e4):
+def loc_beat_midpoints(df, percentile=.999, jump_size_cutoff=5e3):
     """identify timestep midpoints of beats
+
+    timestamp = seconds*sampling_rate
 
     Parameters:
         df (DataFrame): columns \in {timestep, db, abs_db}
+        percentile (float): abs(amplitude) percentile cutoff
         jump_size_cutoff (int): minimum timestep distance between beats
     """
 
-    db_cutoff = df.abs_db.quantile(.99)
+    assert percentile < 1, "percentile must be < 1"
+
+    db_cutoff = df.abs_db.quantile(percentile)
     beat_inds = pd.Series(df[df.abs_db > db_cutoff].index)
 
     timestamp_jump_sizes = abs(np.array(beat_inds - beat_inds.shift(1).iloc[1:], dtype="int32"))
